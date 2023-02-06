a1023eeefaa03c7baf0fc44ba3370571128ed1f9
fpetrakov <jakephin2015@gmail.com>
2023-02-06 17:53:50 +0300

    1 day

--- 1/1.js
+++ 1/1.js
@@ -1,1 +1,18 @@
-// not ready yet
+const fs = require('node:fs')
+const path = require('node:path')
+
+let result = 0
+
+fs.readFileSync(path.resolve(__dirname, '1.txt'))
+	.toString()
+	.split('\n')
+	.reduce((acc, el) => {
+		if (el === '') {
+			result = Math.max(result, acc)
+			return 0
+		}
+
+		return (acc += +el)
+	}, 0)
+
+console.log(result)
