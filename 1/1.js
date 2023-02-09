33b766472e65513443aae50fab7d0dd78d1ce8bc
fpetrakov <jakephin2015@gmail.com>
2023-02-09 21:36:07 +0300

    update 1 day

--- 1/1.js
+++ 1/1.js
@@ -1,18 +1,13 @@
 const fs = require('node:fs')
 const path = require('node:path')
 
-let result = 0
-
-fs.readFileSync(path.resolve(__dirname, '1.txt'))
+const sums = fs
+	.readFileSync(path.resolve(__dirname, '1.txt'))
 	.toString()
-	.split('\n')
-	.reduce((acc, el) => {
-		if (el === '') {
-			result = Math.max(result, acc)
-			return 0
-		}
+	.split('\n\n')
+	.map(line => line.split('\n').map(Number))
+	.map(nums => nums.reduce((a, b) => a + b))
 
-		return (acc += +el)
-	}, 0)
+const result = Math.max(...sums)
 
 console.log(result)
